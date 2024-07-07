be76944e4167053b89839edebf833f4139b74f12
doro404 <166941040+doro404@users.noreply.github.com>
2024-07-06 22:35:16 -0300

    Add files via upload

--- botnews.py
+++ botnews.py
@@ -1,4 +1,5 @@
 # -- coding: utf-8 --
+import asyncio
 import requests
 from telegram import Bot
 
@@ -8,13 +9,13 @@ bot_token = '7316357488:AAHQbiCSpCqrDZgmfi25vJs2roXInS1aFCU'  # Substitua pelo s
 # ID do canal para onde a mensagem será enviada (deve começar com @ para canais públicos)
 channel_id = '@canalontste0'  # Substitua pelo ID do seu canal
 
-# Função para enviar uma mensagem para o canal no Telegram
-def enviar_mensagem_no_canal(mensagem):
+# Função assíncrona para enviar uma mensagem para o canal no Telegram
+async def enviar_mensagem_no_canal(mensagem):
     bot = Bot(token=bot_token)
-    bot.send_message(chat_id=channel_id, text=mensagem)
+    await bot.send_message(chat_id=channel_id, text=mensagem)
 
 # Função para buscar e enviar detalhes dos animes lançados hoje
-def enviar_detalhes_animes_lancados_hoje():
+async def enviar_detalhes_animes_lancados_hoje():
     try:
         # Realizar uma solicitação GET para a rota /animes-lancados-hoje da sua aplicação
         response = requests.get('https://saikanet.online:3000/animes-lancados-hoje')
@@ -38,7 +39,7 @@ def enviar_detalhes_animes_lancados_hoje():
                                     sinopse=anime["sinopse"])
 
                     # Enviar mensagem para o canal no Telegram
-                    enviar_mensagem_no_canal(mensagem)
+                    await enviar_mensagem_no_canal(mensagem)
 
                 print('Detalhes dos animes lançados hoje enviados com sucesso para o canal!')
             else:
@@ -48,5 +49,10 @@ def enviar_detalhes_animes_lancados_hoje():
     except Exception as e:
         print('Erro ao buscar ou enviar detalhes dos animes lançados hoje:', str(e))
 
-# Chamada da função para buscar e enviar detalhes dos animes lançados hoje
-enviar_detalhes_animes_lancados_hoje()
+# Função principal para iniciar o processo assíncrono
+async def main():
+    await enviar_detalhes_animes_lancados_hoje()
+
+# Iniciar o loop de eventos assíncronos
+if __name__ == '__main__':
+    asyncio.run(main())
